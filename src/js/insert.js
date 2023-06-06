108ed38c2356a918d683e6e7b54091a28ba058a7
JSanLazaro <jsanlazaro@hotmail.com>
2023-06-06 20:26:50 +0200

    various changes on cloud js

--- src/js/insert.js
+++ src/js/insert.js
@@ -3,25 +3,18 @@ const btnModalOpen = document.querySelector("#btn_modal_open");
 btnModalOpen.addEventListener("click", () => modal.showModal());
 const btnModalClose = document.querySelector("#btn_modal_close");
 btnModalClose.addEventListener("click", () => modal.close());
+
 const modalWinner = document.querySelector("#modal_winner");
 const btnModalWinner = document.querySelector("#winner_button");
-btnModalWinner.addEventListener("click", () => {
-   
-  if (cloudObject.texts.length==1){
-    winnerInject(); 
-    modalWinner.showModal();
-  }
-   
-});
 
-const btnModalwinnerClose = document.querySelector(
-  "#modal_winner__close_button"
-);
+
+const btnModalwinnerClose = document.querySelector("#modal_winner__close_button");
 btnModalwinnerClose.addEventListener("click", () => modalWinner.close());
-function winnerInject() {
+function winnerInject(name) {
   const modalWinnerMessage = document.getElementById("modal_winner__message");
+  modalWinnerMessage.innerHTML = "<b>EL GANADOR ES</b>";
   const winnerName = document.createElement("span");
-  winnerName.innerText = " "+cloudObject.getActualName();
+  winnerName.innerText = " "+ name;
   modalWinnerMessage.append(winnerName);
 }
 
@@ -51,51 +44,69 @@ function getContainerElement() {
 }
 const startButton = document.getElementById("start_button");
 startButton.addEventListener("click", () => {
-  cloudObject.start();
+  cloudObject.start(callbackFunction);  
 });
-const whoButton = document.getElementById("who_button");
-whoButton.addEventListener("click", () => {
-  alert(cloudObject.getActualName());
-  cloudObject.eraseActualIndexFromList();
-  cloudObject.refreshCloud();
-});
+function callbackFunction(){
+  if(cloudObject.texts.length>1){
+    alert(cloudObject.getActualName());
+  eliminarJugador(cloudObject.actualIndex);
+  }
+  if(cloudObject.texts.length==1){
+    winnerInject(cloudObject.texts[0]); 
+    modalWinner.showModal();
+  }
+  
+}
+// const whoButton = document.getElementById("who_button");
+// whoButton.addEventListener("click", () => {
+//   alert(cloudObject.getActualName());
+//   cloudObject.eraseActualIndexFromList();
+//   cloudObject.refreshCloud();
+//   eliminarJugador(cloudObject.actualIndex);
+// });
 
 // Array para almacenar la lista de jugadores
-let jugadores = [];
+// let jugadores = [];
 
 // Función para agregar un jugador
 function agregarJugador() {
-  alert("agregando jugador");
+  // alert("agregando jugador");
   let nombreInput = document.getElementById("nombre");
   let nombre = nombreInput.value;
 
   // Verificar si el nombre no está vacío
-  if (nombre !== "") {
+  if (nombre !== "" && !cloudObject.contain(nombre)) {
     // Crear objeto jugador
-    let jugador = {
-      nombre: nombre,
-    };
+    // let jugador = {
+    //   nombre: nombre,
+    // };
 
     // Agregar el jugador al array
-    jugadores.push(jugador);
+    // jugadores.push(jugador);
 
     cloudObject.addNameList(nombre);
-    cloudObject.refreshCloud();
+    
 
     // Limpiar el campo de nombre
     nombreInput.value = "";
+    nombreInput.focus();
 
     // Actualizar la lista de jugadores
+    cloudObject.refreshCloud();
     actualizarListaJugadores();
+    
   }
 }
 
 // Función para eliminar un jugador
 function eliminarJugador(index) {
   // Eliminar el jugador del array
-  jugadores.splice(index, 1);
+  // jugadores.splice(index, 1);
+  cloudObject.deleteByIndexList(index);
+  
 
   // Actualizar la lista de jugadores
+  cloudObject.refreshCloud();
   actualizarListaJugadores();
 }
 
@@ -106,9 +117,11 @@ function editarJugador(index) {
   // Verificar si se ingresó un nuevo nombre
   if (nuevoNombre !== null && nuevoNombre !== "") {
     // Actualizar el nombre del jugador en el array
-    jugadores[index].nombre = nuevoNombre;
+    // jugadores[index].nombre = nuevoNombre;
+    cloudObject.texts[index] = nuevoNombre;
 
     // Actualizar la lista de jugadores
+    cloudObject.refreshCloud();
     actualizarListaJugadores();
   }
 }
@@ -119,12 +132,12 @@ function actualizarListaJugadores() {
   listaJugadores.innerHTML = "";
 
   // Recorrer el array de jugadores y crear elementos de lista para cada uno
-  for (let i = 0; i < jugadores.length; i++) {
-    let jugador = jugadores[i];
+  for (let i = 0; i < cloudObject.texts.length; i++) {
+    let nombre = cloudObject.texts[i];
 
     let itemLista = document.createElement("div");
     itemLista.innerHTML =
-      jugador.nombre +
+      nombre +
       " " +
       '<button onclick="eliminarJugador(' +
       i +
@@ -136,3 +149,12 @@ function actualizarListaJugadores() {
     listaJugadores.appendChild(itemLista);
   }
 }
+
+document.getElementById("nombre")
+    .addEventListener("keydown", function(event) {
+    // event.preventDefault();
+    if (event.keyCode === 13) {
+      // alert("enter pressed");
+        agregarJugador();
+    }
+});
